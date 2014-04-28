dabc961609fdb52c2e85b673a0f7a2b984e76d24
Jerome Lachaud <jerome.lachaud@gmail.com>
2014-04-28 20:13:27 +0200

    Site Map

--- template-home.php
+++ template-home.php
@@ -1,10 +1,10 @@
-<?php /* Template Name: Demo Page Template */ get_header(); ?>
+<?php /* Template Name: Home Page Template */ get_header(); ?>
 
 	<main role="main">
 		<!-- section -->
 		<section>
 
-			<h1><?php the_title(); ?></h1>
+			<!-- <h1>< ?php the_title(); ?></h1> -->
 
 		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
@@ -37,7 +37,5 @@
 		</section>
 		<!-- /section -->
 	</main>
-
-<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
