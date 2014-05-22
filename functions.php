e0ac749fb29ff7c60a5a3ce309198a681590e686
Jerome Lachaud <jerome.lachaud@gmail.com>
2014-05-22 17:34:57 +0200

    add 0 as an option in the number of column select list

--- functions.php
+++ functions.php
@@ -591,4 +591,47 @@ function spp_html_sitemap() {
 }
 add_shortcode( 'spp-sitemap','spp_html_sitemap' );
 
+/*
+ * Plugin Name: Media Gallery Zero Columns
+ * Plugin URI: http://www.wizzud.com/
+ * Description: Adds an option to select 0 (zero) columns when creating/editing a gallery in the Media Manager (v3.5+)
+ * Version: 1.0.0
+ * Author: Roger Barrett
+ * Author URI: http://www.wizzud.com/
+ * License: GPL2+
+*/
+
+// 3.5+ media gallery...
+add_action( 'wp_enqueue_media', 'mgzc_media_gallery_zero_columns' );
+function mgzc_media_gallery_zero_columns(){
+    add_action( 'admin_print_footer_scripts', 'mgzc_media_gallery_zero_columns_script', 999);
+}
+function mgzc_media_gallery_zero_columns_script(){
+?>
+<script type="text/javascript">
+jQuery(function(){
+    if(wp.media.view.Settings.Gallery){
+        wp.media.view.Settings.Gallery = wp.media.view.Settings.extend({
+            className: "gallery-settings",
+            template: wp.media.template("gallery-settings"),
+            render: function() {
+                wp.media.View.prototype.render.apply( this, arguments );
+                // Append an option for 0 (zero) columns if not already present...
+                var $s = this.$('select.columns');
+                if(!$s.find('option[value="0"]').length){
+                    $s.append('<option value="0">0</option>');
+                }
+                // Select the correct values.
+                _( this.model.attributes ).chain().keys().each( this.update, this );
+                return this;
+            }
+        });
+    }
+});
+</script>';
+<?php
+}
+
+
+
 ?>
